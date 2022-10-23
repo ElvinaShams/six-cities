169493f399fdb52cfe463b13466c0f9c33a83575
Elvina shamsutdinova <kuku1802shka@mail.ru>
2022-10-23 10:54:41 +0300

    Создание компонентов

---
+++ project/src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app/app';
+
+const Setting = {
+  PLACES_COUNT: 312,
+};
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+
+root.render(
+  <React.StrictMode>
+    <App placesCount={Setting.PLACES_COUNT}/>
+  </React.StrictMode>,
+);
---
+++ project/src/pages/PropertyNotLogged/index.ts
@@ -0,0 +1,1 @@
+export { PropertyNotLogged } from './PropertyNotLogged';
